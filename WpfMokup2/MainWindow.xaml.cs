99aa9f8e3f3165bf60a94bfda111096ff320910a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-18 13:20:48 +0100

    Add project files.

---
+++ WpfMokup2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMokup2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPrimoNum.IsEnabled = true;
+            txtSecondoNum.IsEnabled = true;
+            sldPrimoNum.IsEnabled = true;
+            sldSecondoNum.IsEnabled = true;
+            txtSum.IsEnabled = true;
+        }
+        private void CheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {
+            txtPrimoNum.IsEnabled = false;
+            txtSecondoNum.IsEnabled = false;
+            sldPrimoNum.IsEnabled = false;
+            sldSecondoNum.IsEnabled = false;
+            txtSum.IsEnabled = false;
+        }
+
+        private void sldPrimoNum_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double n1 = (double)sldPrimoNum.Value;
+            double n2 = (double)sldSecondoNum.Value;
+
+            double somma = n1 + n2;
+            txtSum.Text = somma.ToString();
+
+        }
+    }
+}
